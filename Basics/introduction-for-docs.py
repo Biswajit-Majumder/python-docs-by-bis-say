e350f3510e00113c841bc66c381006ba208307df
Biswajit <biswajitmajumderjee@gmail.com>
2023-05-05 19:13:10 +0530

    first commit

---
+++ Basics/introduction-for-docs.py
@@ -0,0 +1,83 @@
+# python 
+
+
+#  Python is a high level( high level languages are those languages thar are easily understandable and readable for programmers and uses natural language construcs and abstructions to hide low level details ) , interpret ( that gets executed by interpreter means there is no compiler to complie the code before hence can be used to see results real time executes programme line by line ) , interactive () , object oriented , open sourced programming language.
+
+# It was created by Guido van Rossum, and released in 1991.
+
+# Python relies on indentation, using whitespace, to define scope; such as the scope of loops, functions and classes. Other programming languages often use curly-brackets for this purpose.
+
+#  Python is designed to be highly readable.
+#  It uses English keywords frequently where as other languages use punctuation , and it has fewer syntactical constructions than other languages.
+#  Python is a MUST for students and working professionals to become a great Software Engineer specially when they are working in Web Development Domain.
+#  Python is a programming language that lets you work quickly and integrate systems more efficiently.
+#  There are two major Python versions Python 2 and Python 3. Both are quite different.
+#  Python 2.x is legacy , Python 3.x is the present and future of the language.
+#  Python 3.0 was released in 2008. The final 2.x version 2.7 release came out in mid 2010 , with a statement of extended support for this end-of-life release.
+#  Python 2.7 will not be maintained past 2020.
+#  Python 3.0 was designed to be backwards incompatible , many projects have not yet made the switch , as it would involve rewriting tens of thousands of lines of code.
+#  Python 3.0 makes breaking changes to the language , but many projects have been slow to port their code to the new version , or are prevented from doing so by their dependencies.
+
+# Where do we use python ? 
+
+# 1. Web Development : Python can be used to make web applications at many levels of complexity.
+# 2. Game Development : Python is also used in development of interactive games.
+# 3. Machine Learning and Artificial Intelligence : Python is widely used in AI , ML and Deep Learning.
+# 4. Data Science and Data Visualization : Python is used to perform data analysis , create beautiful visualizations and perform machine learning.
+# 5. Desktop GUI : Python is used to build desktop applications.
+# 6. Web Scraping Applications : Python is used to build web scraping applications to extract data from websites.
+# 7. Business Applications : Python is also used to build applications that do business related tasks.
+# 8. Audio and Video Applications : Python is used to build applications that can read , write and play sound and videos.
+# 9. Embedded Applications : Python is also used in embedded applications.
+# 10. 3D CAD Applications : Python is also used in 3D CAD applications.
+
+
+
+# python is a dynamically types language that ..... here answer dynamically typed vs static stype
+
+
+
+# Go to python.org to download python
+
+# what is precedence ------ > precedence is the condition of giving importance to someone over someone (the condition of being considered more important than someone or something else; priority in importance, order, or rank. definition -- google )
+
+# now download a desired ide for code writting editing and manupulating
+
+# now make a folder where you will keep you python files hirarchyly and it will keep your files clean
+
+#  then make a file called programmehelloworld.py
+
+#! notice that you have to write the .py extension after the python file it's a convention ( conventions are widely known or accepted standars, practices or rules that are follewed in a pratical field or groud of people or community or )
+
+# Python Indentation
+
+# Indentation refers to the spaces at the beginning of a code line.
+
+# Where in other programming languages the indentation in code is for readability only, the indentation in Python is very important.
+
+# Python uses indentation to indicate a block of code.
+
+# first write this instruction into your text editor 
+
+print("Hello, World!")
+
+# now write the smallest expression 
+# 2 + 2 
+
+
+# why python is popular 
+# 1 for it's design philosophy ( readability , well indented )
+# 2 batteries included <fun turm> ( many function are already there to do the tasks smoothly we don't have to re-invent the wheel they are abstructed away by python inbuilt  --- )
+# 3 free and open source ( python is free and open source and it's community is very large and active )
+# 4 portability ( python is portable means it can be run on any platform like windows , linux , mac , raspberry pi etc. )
+# 5 general purpose ( supports many programming paradigms we can make many things )
+
+
+
+# python is case sensitive
+
+# now let's understand what we are doing here 
+# we are using a function called print() to print something on the screen
+# print() is a function that takes a string as an argument and prints it on the screen
+# a string is a sequence of characters enclosed in single or double quotes
+# print() is a built-in function in python
